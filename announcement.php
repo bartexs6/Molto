fac0974094e732206d4cfdca5609d4efc7ee2345
bartexs6 <50320735+bartexs6@users.noreply.github.com>
2021-10-29 21:00:22 +0200

    Walidacja danych

--- announcement.php
+++ announcement.php
@@ -1,6 +1,12 @@
 <?php
 class Announcement{
 
+    const MAX_TITLE_LENGTH = 21;
+    const MAX_DESCRIPTION_LENGTH = 33;
+    const MAX_IMGLINK_LENGTH = 65;
+    const MAX_CONTACT_LENGTH = 65;
+    const MAX_LOCATION_LENGTH = 65;
+
     private $id;
     public $category;
     public $title;
@@ -29,18 +35,36 @@ class Announcement{
 
     // Dodawanie ogloszen do bazy
     public static function addAnnouncement($category, $title, $description, $value, $img_link, $contact, $location, $date, $user_owner){
+        if(empty($category) || empty($title) || empty($description) || empty($value) || empty($img_link) || empty($contact) || empty($location) || empty($date) || empty($user_owner)){
+            throw new Exception("Empty variable(s)", 1);
+        }
 
-        $conn = DatabaseConnect::connect();
-        $cmd = mysqli_prepare($conn, "INSERT INTO `announcement`(`category`, `title`, `description`, `value`, `img_link`, `contact`, `location`, `date`, `user_owner`) VALUES (?,?,?,?,?,?,?,?,?)");
+        if(!Announcement::validateDate($date)){
+            throw new Exception("Date error", 1);
+        }
 
-        mysqli_stmt_bind_param($cmd, "sssissssi", $category, $title, $description, $value, $img_link, $contact, $location, $date, $user_owner);
-        mysqli_stmt_execute($cmd);
+        if(strlen($title) < Announcement::MAX_TITLE_LENGTH && strlen($description) < Announcement::MAX_DESCRIPTION_LENGTH && strlen($img_link) < Announcement::MAX_IMGLINK_LENGTH && strlen($contact) < Announcement::MAX_CONTACT_LENGTH && strlen($location) < Announcement::MAX_LOCATION_LENGTH){
+            $conn = DatabaseConnect::connect();
+            $cmd = mysqli_prepare($conn, "INSERT INTO `announcement`(`category`, `title`, `description`, `value`, `img_link`, `contact`, `location`, `date`, `user_owner`) VALUES (?,?,?,?,?,?,?,?,?)");
+    
+            mysqli_stmt_bind_param($cmd, "sssissssi", $category, $title, $description, $value, $img_link, $contact, $location, $date, $user_owner);
+            mysqli_stmt_execute($cmd);
+    
+            $getResult = mysqli_stmt_get_result($cmd);
+    
+            mysqli_close($conn);
+    
+            //return new Announcement($row[0],$row[1],$row[2],$row[3],$row[4],$row[5],$row[6],$row[7],$row[8],$row[9]);
+        }else{
+            throw new Exception("Too long variable(s)", 1);
+        }
+    }
 
-        $getResult = mysqli_stmt_get_result($cmd);
-
-        mysqli_close($conn);
-
-        //return new Announcement($row[0],$row[1],$row[2],$row[3],$row[4],$row[5],$row[6],$row[7],$row[8],$row[9]);
+    // Sprawdzanie daty
+    static function validateDate($date, $format = 'Y-m-d H:i:s')
+    {
+        $d = DateTime::createFromFormat($format, $date);
+        return $d && $d->format($format) == $date;
     }
 
     // Pobieranie ogloszenia poprzez id
@@ -57,7 +81,11 @@ class Announcement{
 
         mysqli_close($conn);
 
-        return new Announcement($row[0],$row[1],$row[2],$row[3],$row[4],$row[5],$row[6],$row[7],$row[8],$row[9]);
+        if(isset($row)){
+            return new Announcement($row[0],$row[1],$row[2],$row[3],$row[4],$row[5],$row[6],$row[7],$row[8],$row[9]);
+        }else{
+            throw new Exception("Cannot find announcement", 1);
+        }
     }
 
     // Pobieranie losowego ogloszenia (Przykladowe uzycie: Strona glowna)
@@ -73,7 +101,6 @@ class Announcement{
 
         return Announcement::getById($randomId);
     }
-
     // INSERT INTO `announcement`(`id`, `category`, `title`, `description`, `value`, `img_link`, `contact`, `location`, `date`, `user_owner`) VALUES (null,"elektronika","asda","dgfdsgd",13,"/brak.png","asdsadad","dsfdsfdsf","2021-10-10",1)
 }
 ?>
--- user.php
+++ user.php
@@ -1,6 +1,10 @@
 <?php
 class User{
 
+    const MAX_USERNAME_LENGTH = 21;
+    const MAX_PASSWORD_LENGTH = 33;
+    const MAX_EMAIL_LENGTH = 65;
+
     private $id;
     private $password;
     public $username;
@@ -8,21 +12,46 @@ class User{
 
     // Dodawanie uzytkownika
     public static function addUser($username, $password, $email){
-        $conn = DatabaseConnect::connect();
-        $cmd = mysqli_prepare($conn, "INSERT INTO `user`(`username`, `password`, `email`) VALUES (?,?,?)");
+        if(empty($username) || empty($password) || empty($email)){
+            throw new Exception("Empty username, password or email", 1);
+        }
 
-        $password = md5($password);
+        if(User::checkData($username, $email)){
+            throw new Exception("Username or email exists", 1);
+        }
 
-        mysqli_stmt_bind_param($cmd, "sss", $username, $password, $email);
-        mysqli_stmt_execute($cmd);
+        if(strlen($username) < User::MAX_USERNAME_LENGTH && strlen($password) < User::MAX_PASSWORD_LENGTH && strlen($email) < User::MAX_EMAIL_LENGTH ){
+            if(ctype_alnum($username) && filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $conn = DatabaseConnect::connect();
+                $cmd = mysqli_prepare($conn, "INSERT INTO `user`(`username`, `password`, `email`) VALUES (?,?,?)");
+        
+                $password = md5($password);
+        
+                mysqli_stmt_bind_param($cmd, "sss", $username, $password, $email);
+                mysqli_stmt_execute($cmd);
+        
+                $getResult = mysqli_stmt_get_result($cmd);
+        
+                mysqli_close($conn);
 
-        $getResult = mysqli_stmt_get_result($cmd);
-
-        mysqli_close($conn);
+            }else{
+                throw new Exception("Incorrect email or username", 1);
+            }
+        }else{
+            throw new Exception("Too long username, password or email", 1);
+        }
     }
 
     // Sprawdzanie poprawnosci danych
     public static function loginValidate($username, $password) : bool{
+        if(empty($username) || empty($password)){
+            return false;
+        }
+
+        if(strlen($username) > User::MAX_USERNAME_LENGTH && strlen($password) > User::MAX_PASSWORD_LENGTH){
+            return false;
+        }
+
         $conn = DatabaseConnect::connect();
         $cmd = mysqli_prepare($conn, "SELECT password FROM user WHERE username=?");
 
@@ -52,6 +81,26 @@ class User{
             session_regenerate_id();
             $_SESSION['logged'] = TRUE;
             $_SESSION['username'] = $username;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // Sprawdzanie czy dane istnieja juz w bazie
+    private static function checkData($username, $email){
+        $conn = DatabaseConnect::connect();
+        $cmd = mysqli_prepare($conn, "SELECT username,email FROM user WHERE username=? or email=?");
+
+        mysqli_stmt_bind_param($cmd, "ss", $username, $email);
+        mysqli_stmt_execute($cmd);
+
+        $getResult = mysqli_stmt_get_result($cmd);
+ 
+        $row = mysqli_fetch_row($getResult);
+        mysqli_close($conn);
+
+        if(isset($row[0])){
             return true;
         }else{
             return false;
