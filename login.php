4af2d344d8e0490777527f963303c76686b022ac
Geq0nek <93310012+Geq0nek@users.noreply.github.com>
2021-10-31 23:55:16 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Molto - konto</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+
+    <div class="popup_form">
+        <div class="forms">
+            <button type="button" class="button">LOGIN</button>
+            <button type="button" class="button">SIGN UP</button>
+        </div>
+
+        <div class="login_section">
+            <form action="" method="">
+                <label for="email">Email</label><br>
+                    <input type="email" required><br>
+                <label for="password">Password</label><br>
+                    <input type="password" required><br>
+                <button type="submit">LOGIN</button>
+            </form>
+        </div>
+
+    </div>
+
+</body>
+</html>
