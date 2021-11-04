e16b9e049cb0de325b504f9f47544748ee7911f7
bartexs6 <50320735+bartexs6@users.noreply.github.com>
2021-11-04 08:10:49 +0100

    Zmiany w stylach

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Molto</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="style/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 </head>
 
@@ -52,7 +52,7 @@ include_once("user.php");
 include_once("announcement.php");
 include_once("error.php");
 
-echo '<link href="styl.css" rel="stylesheet">';
+echo '<link href="style/styl.css" rel="stylesheet">';
 
 User::login("ada", "test");
 if(isset($_SESSION['logged']) && $_SESSION['logged'] == TRUE){
