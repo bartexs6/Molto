a16753b808ee7416042992825abceb0c95423e30
Geq0nek <93310012+Geq0nek@users.noreply.github.com>
2021-11-12 13:38:53 +0100

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -12,57 +12,70 @@
         <div class="navigation">  
             <ul>
                 <li>
-                    <a href="#">
+                    <button class="tab-btn" data-for-tab="1">
                         <span class="icon"><ion-icon name="menu-outline"></ion-icon></span>
                         <span class="title">Menu</span>
-                    </a>
+                    </button>
                 </li>
                 <li>
-                    <a href="#">
+                    <button class="tab-btn" data-for-tab="2">
                         <span class="icon"><ion-icon name="person-outline"></ion-icon></span>
                         <span class="title">Konto</span>
-                    </a>
+                    </button>
                 </li>
                 <li>
-                    <a href="#">
+                    <button class="tab-btn" data-for-tab="3">
                         <span class="icon"><ion-icon name="heart-outline"></ion-icon></span>
                         <span class="title">Ulubione</span>
-                    </a>
+                    </button>
                 </li>
                 <li>
-                    <a href="#">
+                    <button class="tab-btn tab-btn-active" data-for-tab="4">
                         <span class="icon"><ion-icon name="cart-outline"></ion-icon></span>
                         <span class="title">Ogłoszenia</span>
-                    </a>
+                    </button>
                 </li>
                 <li>
-                    <a href="#">
+                    <button class="tab-btn" data-for-tab="5">
                         <span class="icon"><ion-icon name="help-outline"></ion-icon></span>
                         <span class="title">Pomoc</span>
-                    </a>
+                    </button>
                 </li>
                 <li>
-                    <a href="#">
+                    <button class="tab-btn" data-for-tab="6">
                         <span class="icon"><ion-icon name="log-out-outline"></ion-icon></span>
                         <span class="title">Wyloguj się</span>
-                    </a>
+                    </button>
                 </li>
             </ul>
         </div>
 
-        <div class="dasOption">
+        <div class="content">
+            <!--MENU-->
+            <div class="tab-content" data-tab="1">1
+            </div>
             <!--KOTNO-->
-                
-            <!--USTAWIENIA-->
+            <div class="tab-content" data-tab="2">2
+            </div>
 
+            <!--ULUBIONE-->
+            <div class="tab-content" data-tab="3">3
+            </div>
+    
             <!--OGŁOSZENIA-->
-                <div class="announcements">
+                <div class="tab-content tab-content-active" data-tab="4">
                     <?php
                         include "addAnnForm.php";
                     ?>
                 </div>
             <!--POMOC-->
-            
+            <div class="tab-content" data-tab="5">5
+            </div>
+    
+            <!--wYLOGUJ SIĘ-->
+            <div class="tab-content" data-tab="6">6
+            </div>
+
         </div>
 
     </div>
@@ -70,6 +83,13 @@
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
+    <script>
+        //AKTYWNE OKNA DASHBOARD'U
+        function setupTabs (){
+        
+
+        }
+    </script>
 
 </body>
 
